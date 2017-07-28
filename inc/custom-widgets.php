3957aa1b2fb68a10b78371fa41b09bfd216e5b78
vkbidhan <bidhanvk@gmail.com>
2017-07-28 21:58:13 +0600

    custom widget class declear here

--- inc/custom-widgets.php
+++ inc/custom-widgets.php
@@ -1,1 +1,17 @@
 <?php
+    class ledpCustomWidgets extends WP_Widget{
+        public function __construct(){
+             parent::__construct(
+                'ledpwidget','Ledp Widget',array(
+                    'description' => 'This widget for ledp porject footer option',
+                )
+             );
+        }
+
+        public function widget($args,$value){
+
+        }
+        public function form($value){
+
+        }
+    }
